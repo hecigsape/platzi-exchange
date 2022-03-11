ae6d21483d8b575cce6b9d2c5e367af2f9f50075
Hector Sanabria <hector.sanabria01@uptc.edu.co>
2022-03-11 07:53:21 -0500

    rutas dinamicas, clase 32, permite generar una ruta con una componente que se modifica  la url

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+//import { getMetadataKeys } from "core-js/fn/reflect";
+
 const url = "https://api.coincap.io/v2";
 
 function getAssets() {
@@ -7,4 +9,24 @@ function getAssets() {
     .then((res) => res.data);
 }
 
-export default { getAssets };
+//permite ver el detalle de las criptos
+function getAsset(coin) {
+  //hace fecht entre la url principal y assets, adema se limita a 20 critos
+  return fetch(`${url}/assets/${coin}`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
+//obtiene el histroial de pprecios
+function getAssetHistory(coin) {
+  const now = new Date();
+  const end = now.getTime();
+  now.setDate(now.getDate() - 1);
+  const start = now.getTime();
+  return fetch(
+    `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`
+  )
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+export default { getAssets, getAsset, getAssetHistory };
