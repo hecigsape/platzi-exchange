9fbb8687d619c4b49c0f5e89a228660af0050b4b
Hector Sanabria <hector.sanabria01@uptc.edu.co>
2022-03-17 10:39:21 -0500

    clase 36, Se agregan filtros y ordenamiento por raking

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,24 @@ import PxHeader from "./components/PxHeader.vue";
 export default {
   name: "App", //este nombre permite hacer un dbug mas facil
   components: { PxHeader },
+  data() {
+    return {
+      links: [
+        {
+          title: "BTC",
+          to: { name: "coin-detail", params: { id: "bitcoin" } },
+        },
+        {
+          title: "ETH",
+          to: { name: "coin-detail", params: { id: "ethereum" } },
+        },
+        {
+          title: "XRP",
+          to: { name: "coin-detail", params: { id: "ripple" } },
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,19 +3,29 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span class="underline cursor-pointer" @click="changeSortorder"
+            >Ranking</span
+          >
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="a in assets"
+        v-for="a in filteredAssets"
         :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -36,8 +46,8 @@
           </router-link>
           <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
         </td>
-        <td>{{ a.priceUsd | dolar }}</td>
-        <td>{{ a.marketCapUsd | dolar }}</td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
         <td
           :class="
             a.changePercent24Hr.includes('-')
@@ -77,13 +87,44 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      filter: "",
+      sortOrder: 1,
+    };
+  },
+  computed: {
+    //permite hacer un filtro que solo muestra coincidencias en la tabla con el nombre o el simbolo
+    filteredAssets() {
+      /*if (!this.filter) {
+        return this.assets;
+      }*/
+      const altOrder = this.sortOrder === 1 ? -1 : 1;
+      return this.assets
+        .filter(
+          (a) =>
+            a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            a.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder;
+          }
+          return altOrder;
+        });
+    },
+  },
   components: {
     PxButton,
   },
+
   methods: {
     goToCoin(id) {
       //permte acceder a la isntancia y me permite navegar
       this.$router.push({ name: "coin-detail", params: { id } });
+    },
+    changeSortorder() {
+      this.sortOrder = this.sortOrder === 1 ? -1 : 1;
     },
   },
 };
